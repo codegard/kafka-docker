d65debecce9e974e5e13868333d1b74252517e83
Mats30 <mati.rataj@gmail.com>
2019-09-02 23:31:12 +0200

    KafkaErrorHandler updated with offset seeking for each partition

--- consumer/src/main/java/com/rataj/kafka/config/KafkaErrorHandler.java
+++ consumer/src/main/java/com/rataj/kafka/config/KafkaErrorHandler.java
@@ -1,5 +1,6 @@
 package com.rataj.kafka.config;
 
+import org.apache.commons.logging.Log;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
@@ -9,35 +10,50 @@ import org.springframework.kafka.listener.ContainerAwareErrorHandler;
 import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.support.serializer.DeserializationException;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiPredicate;
 
 final class KafkaErrorHandler implements ContainerAwareErrorHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(KafkaErrorHandler.class);
-    private static final String KEY_DESERIALIZATION_ERROR_KEY = "springDeserializerExceptionKey";
-    private static final String VALUE_DESERIALIZATION_ERROR_KEY = "springDeserializerExceptionValue";
 
     @Override
     public void handle(Exception thrownException, List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer, MessageListenerContainer container) {
-
-        if (thrownException.getClass().equals(DeserializationException.class)) {
-            records.stream()
-                    .filter(consumerRecord -> !Objects.nonNull(consumerRecord.headers().lastHeader(KEY_DESERIALIZATION_ERROR_KEY))
-                            || !Objects.nonNull(consumerRecord.headers().lastHeader(VALUE_DESERIALIZATION_ERROR_KEY)))
-                    .forEach(consumerRecord -> {
-                        DeserializationException exception = (DeserializationException) thrownException;
-                        String topic = consumerRecord.topic();
-                        long offset = consumerRecord.offset();
-                        int partition = consumerRecord.partition();
-                        TopicPartition topicPartition = new TopicPartition(topic, partition);
-                        consumer.seek(topicPartition, offset + 1L);
-                        String malformedMessage = new String(exception.getData());
-                        LOG.info("Skipping message with topic {} and offset {} " +
-                                "- malformed message: {} , exception: {}", topic, offset, malformedMessage, exception.getLocalizedMessage());
-                    });
+        doSeeks(records, consumer);
+        if (!records.isEmpty()) {
+            ConsumerRecord<?, ?> record = records.get(0);
+            String topic = record.topic();
+            long offset = record.offset();
+            int partition = record.partition();
+            if (thrownException.getClass().equals(DeserializationException.class)) {
+                DeserializationException exception = (DeserializationException) thrownException;
+                String malformedMessage = new String(exception.getData());
+                LOG.info("Skipping message with topic {} and offset {} " +
+                        "- malformed message: {} , exception: {}", topic, offset, malformedMessage, exception.getLocalizedMessage());
+            } else {
+                LOG.info("Skipping message with topic {} - offset {} - partition {} - exception {}", topic, offset, partition, thrownException);
+            }
         } else {
             LOG.info("Consumer exception - cause: {}", thrownException.getMessage());
         }
+    }
+
+    private void doSeeks(List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer) {
+        Map<TopicPartition, Long> partitions = new LinkedHashMap<>();
+        AtomicBoolean first = new AtomicBoolean(true);
+        records.forEach(record ->  {
+            if (first.get()) {
+                partitions.put(new TopicPartition(record.topic(), record.partition()), record.offset() + 1);
+            } else {
+                partitions.computeIfAbsent(new TopicPartition(record.topic(), record.partition()),
+                        offset -> record.offset());
+            }
+            first.set(false);
+        });
+        partitions.forEach(consumer::seek);
     }
 }
